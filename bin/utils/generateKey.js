af755a14a2a4a9f5546694438fdcf827762b6762
Andre <andre@dev-next.com>
2019-03-08 17:24:44 -0300

    :sparkles: Created a key generator

---
+++ bin/utils/generateKey.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const { Entropy, charset64 } = require('entropy-string');
+
+const { log } = console;
+log(chalk`{whiteBright.bold RANDOM KEY GENERATOR}`);
+log('--------------------');
+
+log(chalk`{gray - Reading .env file...}`);
+const env = String(fs.readFileSync('.env'));
+
+log(chalk`{gray - Creating new Key...}`);
+const entropy = new Entropy({ charset: charset64 });
+const key = entropy.token();
+
+const newEnv = env.replace(/SECRET_KEY=.+/, `SECRET_KEY=${key}`);
+log(chalk`{gray - Saving new .env file...}`);
+fs.writeFileSync('.env', newEnv);
+
+log('\n');
+log(chalk`{bold New Key Generated!}`);
+log(chalk`{bgRedBright.bold ${key}}`);
+log('\n');
+log(chalk`{red Please make sure to keep this key safe, ortherwise you won't be able to decrypt your data!}`);
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 routes(app);
 graphQL(app);
 
+if (process.env.SECRET_KEY.length < 20) {
+  throw new Error('Your SECRET_KEY must be greater than 20 characters. Try generate one using `npm run generate`');
+}
+
 logger.debug('Starting API');
 
 module.exports = app;
