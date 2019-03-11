d9ba2559a0de0216006b7122cbced84fd4a4f200
Andre <andre@dev-next.com>
2019-03-11 10:37:05 -0300

    :lock: Replaced createCipher to createCipheriv

--- bin/utils/generateKey.js
+++ bin/utils/generateKey.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
-const { Entropy, charset64 } = require('entropy-string');
+const { Entropy, charset16 } = require('entropy-string');
 
 const { log } = console;
 log(chalk`{whiteBright.bold RANDOM KEY GENERATOR}`);
@@ -10,8 +10,8 @@ log(chalk`{gray - Reading .env file...}`);
 const env = String(fs.readFileSync('.env'));
 
 log(chalk`{gray - Creating new Key...}`);
-const entropy = new Entropy({ charset: charset64 });
-const key = entropy.token();
+const entropy = new Entropy({ charset: charset16 });
+const key = entropy.token().substr(0, 32);
 
 const newEnv = env.replace(/SECRET_KEY=.+/, `SECRET_KEY=${key}`);
 log(chalk`{gray - Saving new .env file...}`);
--- src/utils/cypher.js
+++ src/utils/cypher.js
@@ -1,16 +1,19 @@
-const { createCipher, createDecipher } = require('crypto');
+const { createCipheriv, createDecipheriv, randomBytes } = require('crypto');
 
-const ALGORITHM = 'aes-256-ctr';
+const ALGORITHM = 'aes-256-cbc';
+const IV_LENGTH = 16;
 
 module.exports = {
   encrypt(text) {
-    const cipher = createCipher(ALGORITHM, process.env.SECRET_KEY);
-    const crypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+    const iv = randomBytes(IV_LENGTH / 2).toString('hex');
+    const cipher = createCipheriv(ALGORITHM, process.env.SECRET_KEY, iv);
+    const crypted = iv + cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
     return crypted;
   },
   decrypt(text) {
-    const decipher = createDecipher(ALGORITHM, process.env.SECRET_KEY);
-    const dec = decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+    const iv = text.substr(0, IV_LENGTH);
+    const decipher = createDecipheriv(ALGORITHM, process.env.SECRET_KEY, iv);
+    const dec = decipher.update(text.substr(IV_LENGTH), 'hex', 'utf8') + decipher.final('utf8');
     return dec;
   },
 };
